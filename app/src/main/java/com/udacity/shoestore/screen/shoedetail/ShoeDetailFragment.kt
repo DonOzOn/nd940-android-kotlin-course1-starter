7787a4d44c3167c9e4009657a8656cf33ee46a0d
Thang Le <>
2022-06-09 11:12:54 +0700

    delete comment in xml and add view model for shoe detail

--- app/src/main/java/com/udacity/shoestore/screen/shoedetail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screen/shoedetail/ShoeDetailFragment.kt
@@ -11,15 +11,18 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoeDetailFragmentBinding
 import com.udacity.shoestore.models.Shoe
+import com.udacity.shoestore.screen.shoelist.ShoeListViewModel
 import kotlinx.android.synthetic.main.shoe_detail_fragment.*
 
 
 class ShoeDetailFragment : Fragment() {
-
+    private lateinit var viewModel: ShoeViewModel
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
@@ -28,23 +31,32 @@ class ShoeDetailFragment : Fragment() {
         binding.txtShoeSize.setInputType(InputType.TYPE_CLASS_NUMBER);
         val keyListener: KeyListener = DigitsKeyListener.getInstance("1234567890")
         binding.txtShoeSize.setKeyListener(keyListener)
+        viewModel = ViewModelProviders.of(this).get(ShoeViewModel::class.java);
+        binding.lifecycleOwner = this
         binding.btnSave.setOnClickListener {
             var shoeName: String = binding.txtShoeName2.text.toString();
             var shoeSize: String = binding.txtShoeSize.text.toString();
             var shoeCompany: String = binding.txtCompany.text.toString();
             var shoeDescription: String = binding.txtCompany.text.toString();
+            // check  null
             if(shoeName !== null && shoeSize!== null && shoeCompany !== null && shoeDescription !== null){
                 val newShoe: Shoe = Shoe(binding.txtShoeName2.text.toString()
                     ,binding.txtShoeSize?.text.toString().toDouble()
                     , binding.txtCompany?.text.toString() ,
                     binding.txtDescription.text.toString())
+                viewModel.setShoe(newShoe)
                 Log.i("newShoe", newShoe.toString())
-                findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListDestination(newShoe, true))
             }else{
                 Toast.makeText(this.activity,"Please fill all field", Toast.LENGTH_LONG).show()
             }
 
         }
+
+        // dispatch when set value
+        viewModel.newShoe.observe(viewLifecycleOwner, Observer {shoe ->
+            findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListDestination(
+                shoe, true))
+        })
         binding.btnCancel.setOnClickListener {
             findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListDestination(Shoe("",0.0,"","")))
         }
